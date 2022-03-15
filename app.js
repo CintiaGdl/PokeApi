84b596fab30a2ffac0b564a5db6ee5d1369d1b0d
CintiaGdl <cintiamgori@gmail.com>
2022-03-15 17:43:32 +0100

    add filter

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ window.onload = () => {
     init();
 }
 
+let pokemosMapped = [];
+
 const init = async () => {
     const pokeList = await getPokeList();
-    console.log('pokeList', pokeList);
-    const pokemosMapped = await mappedPoke(pokeList);
-    console.log('pokemosMapped', pokemosMapped);
-
+    // console.log('pokeList', pokeList);
+    pokemosMapped = await mappedPoke(pokeList);
+    // console.log('pokemosMapped', pokemosMapped);
     printPoke(pokemosMapped);
-
 }
+
 
 const getPokeList = async () => {
     let pokemonsArray = [];
@@ -36,12 +37,30 @@ function mappedPoke(pokemos) {
 function printPoke(pokemos) {
     let list = document.createElement('div');
     list.className = 'containerPoke';
-    console.log(pokemos)
+    // console.log(pokemos)
     pokemos.forEach(poke => {
         list.innerHTML += `<div class="card">
         <h3>${poke.name}</h3>
-        <img src=${poke.img} />
+        <img src=${poke.img} class="imageCard"/>
         </div>`
     });
     document.body.appendChild(list);
 }
+
+const inputSearch = document.querySelector('#inputSearch')
+const buttonSearch = document.querySelector('#searchButton')
+
+function deleteContainer() {
+    document.getElementsByClassName('containerPoke')[0].remove();
+}
+
+
+function buttonHandler() {
+    const searchWord = inputSearch.value;
+    console.log(searchWord);
+    pokemosFilter = pokemosMapped.filter(poke => poke.name.includes(searchWord));
+    console.log(pokemosFilter);
+    deleteContainer();
+    printPoke(pokemosFilter);
+}
+buttonSearch.addEventListener('click', buttonHandler);
